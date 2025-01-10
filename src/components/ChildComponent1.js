bb6a2974135c6ae82dab61d1a41632fbbc9b8aba
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-10 16:48:49 +0530

    Update ChildComponent1.js

--- src/components/ChildComponent1.js
+++ src/components/ChildComponent1.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ChildComponent1 = ({setSelectedOption}) => {
   return (
     <div><h3>ChildComponent1</h3>
-        <button onClick={()=>setSelectedOption("option 1")}>Option 1</button></div>
+        <button onClick={()=>setSelectedOption("Option 1")}>Option 1</button></div>
   )
 }
 
