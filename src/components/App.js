0161521d00dce42513510d825c655db3e50286e4
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-10 16:48:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ const App = () => {
   const [selectedOption,setSelectedOption] = useState("");
   return (
     <div className="parent">
-      <h1>Parent Componenet</h1>
+      <h1>Parent Component</h1>
       <ChildComponent1 setSelectedOption={setSelectedOption}/>
       <ChildComponent2 setSelectedOption={setSelectedOption}/>
-      <p>Selected Option:{selectedOption}</p>
+      <p>Selected Option: {selectedOption}</p>
     </div>
   )
 }
